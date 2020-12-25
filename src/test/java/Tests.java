031f55759dec183eaff12b4986d5f4d5c9e02e5b
Anton <antoxzan@gmail.com>
2020-12-25 23:03:53 +0200

    1

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -1,19 +1,18 @@
 import com.jayway.restassured.RestAssured;
-import com.jayway.restassured.response.Response;
 import org.json.JSONObject;
-import org.junit.jupiter.api.Order;
 import org.testng.annotations.Test;
 
 import java.io.File;
 
 import static com.jayway.restassured.config.EncoderConfig.encoderConfig;
 
-public class UploadTest {
+
+public class Tests {
     private final String token = "T_HHsYQ_bv4AAAAAAAAAAU2mw48OB2EfjFEEm-vlkcdUi1nGt3ygT9AvP-GNKSGT";
 
+
     @Test
-    @Order(1)
-    public void uploadTest() {
+    public void test1upload() {
         String filename = "FeelsDankMan.png";
         File file = new File("src/main/resources/"+filename);
         JSONObject jsonObject = new JSONObject();
@@ -30,4 +29,33 @@ public class UploadTest {
                 .then().statusCode(200);
     }
 
+
+    @Test
+    public void test2metadata() {
+        String filename = "FeelsDankMan.png";
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("path", "/test/" + filename);
+
+        RestAssured.given()
+                .header("Authorization", "Bearer " + token)
+                .contentType("application/json")
+                .body(jsonObject.toString())
+                .post("https://api.dropboxapi.com/2/files/get_metadata")
+                .then().statusCode(200);
+    }
+
+
+    @Test
+    public void test3delete() {
+        String filename = "FeelsDankMan.png";
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("path", "/test/" + filename);
+
+        RestAssured.given()
+                .header("Authorization", "Bearer " + token)
+                .contentType("application/json")
+                .body(jsonObject.toString())
+                .post("https://api.dropboxapi.com/2/files/delete_v2")
+                .then().statusCode(200);
+    }
 }
