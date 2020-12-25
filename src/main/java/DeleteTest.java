0e46bd758e06bb1ce38e1c578efb3d67d59ad0a0
Anton <antoxzan@gmail.com>
2020-12-25 14:58:48 +0200

    Initial commit

---
+++ src/main/java/DeleteTest.java
@@ -0,0 +1,24 @@
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.response.Response;
+import org.json.JSONObject;
+import org.testng.annotations.Test;
+
+public class DeleteTest {
+    private final String token = "T_HHsYQ_bv4AAAAAAAAAAU2mw48OB2EfjFEEm-vlkcdUi1nGt3ygT9AvP-GNKSGT";
+
+    @Test
+    public void deleteTest() {
+        String filename = "FeelsDankMan.png";
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("path", "/test/" + filename);
+
+        Response response = RestAssured.given()
+                .header("Authorization", "Bearer " + token)
+                .contentType("application/json")
+                .body(jsonObject.toString())
+                .post("https://api.dropboxapi.com/2/files/delete_v2\n")
+                .thenReturn();
+
+        response.prettyPeek();
+    }
+}
---
+++ src/main/java/UploadTest.java
@@ -0,0 +1,33 @@
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.response.Response;
+import org.json.JSONObject;
+import org.testng.annotations.Test;
+
+import java.io.File;
+
+import static com.jayway.restassured.config.EncoderConfig.encoderConfig;
+
+public class UploadTest {
+    private final String token = "T_HHsYQ_bv4AAAAAAAAAAU2mw48OB2EfjFEEm-vlkcdUi1nGt3ygT9AvP-GNKSGT";
+
+    @Test
+    public void uploadTest() {
+        String filename = "FeelsDankMan.png";
+        File file = new File("src/main/resources/"+filename);
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("path", "/test/"+filename);
+
+        RestAssured.config = RestAssured.config().encoderConfig(encoderConfig().appendDefaultContentCharsetToContentTypeIfUndefined(false));
+        Response response = RestAssured.given()
+                .header("Authorization", "Bearer " + token)
+                .header("Dropbox-API-Arg", jsonObject.toString())
+                .header("Content-Type","application/octet-stream")
+                .contentType("application/octet-stream")
+                .body(file)
+                .post("https://content.dropboxapi.com/2/files/upload")
+                .thenReturn();
+
+        response.prettyPeek();
+    }
+
+}
